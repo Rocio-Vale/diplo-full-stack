f6be74166a6f9d2dc06ad391bce80759ab9a9124
Rocío Vale <rociovale25@gmail.com>
2025-11-21 19:06:09 -0300

    javascript parte 2

---
+++ modulo3-u3/ej1.js
@@ -0,0 +1,8 @@
+const nota1 = 8;
+const nota2 = 5;
+
+function promedio (n1, n2){
+    return (n1+n2)/2;
+}
+
+console.log(`El promedio de las notas ${nota1} y ${nota2} es ${promedio(nota1,nota2)}`);
---
+++ modulo3-u3/ej2.js
@@ -0,0 +1,16 @@
+const cuadroDeTexto = document.getElementById("texto");
+const cantCarac = document.getElementById("cantCaracteres")
+
+let contador = 0;
+
+cant = cuadroDeTexto.addEventListener("keyup", () => {
+    if (cuadroDeTexto.value != "") {
+        contador += 1;        
+    }else {
+        contador = 0;
+    }
+    cantCarac.innerText = contador;
+})
+
+
+
